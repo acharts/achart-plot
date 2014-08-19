5e50ad177aaeb071a934662ba99e4d273a4c7a99
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2014-08-19 15:28:16 +0800

    delete modules

--- spm_modules/achart-canvas/1.0.0/index.js
+++
@@ -1,6 +0,0 @@
-var Canvas = require('./src/canvas');
-
-Canvas.Group = require('./src/Group');
-Canvas.Shape = require('./src/Shape');
-
-module.exports = Canvas;
--- spm_modules/achart-util/1.0.0/index.js
+++
@@ -1,3 +0,0 @@
-var achartUtil = require('./src/util');
-
-module.exports = achartUtil;
